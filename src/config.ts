02052b18d46239e742a53ca901a12c47c7b7b47b
Adrien-Ba <adrien.bassail@live.fr>
2022-01-15 13:11:00 +0100

    Update Backend URL :wrench:

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_URL =
-  `${import.meta.env.VITE_BACKEND_URL_URL}` ?? 'http://localhost:3000/';
+  `${import.meta.env.VITE_BACKEND_URL_URL}` ?? 'https://protected-mesa-89495.herokuapp.com/';
